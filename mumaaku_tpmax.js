95368a05402bc93cefa5d365dc4d38fa7cb97c5a
ショウマ <9rasjk2wxpa6@gmail.com>
2020-09-26 19:39:43 +0900

    Add files via upload

---
+++ mumaaku_tpmax.js
@@ -0,0 +1,29 @@
+﻿//=============================================================================
+// mumaaku_tpmax.js
+// PUBLIC DOMAIN
+//=============================================================================
+/*:
+ * @target MZ
+ * @url https://wiki3.jp/botbot
+ * @plugindesc TPMAXを変更
+ * @author 夢魔 渥(むま あく)
+ * 
+ * @param tpmax
+ * @desc TPの最大値を変更します。(設定は100)
+ * @default 100
+ * @type number
+ * 
+ * @help
+ * パーティのTPの最大値を変更します。
+ */
+
+(function() {
+
+  var parameters = PluginManager.parameters('mumaaku_tpmax');
+  var tpmax = String(parameters["tpmax"]);
+
+Game_BattlerBase.prototype.maxTp = function() {
+    return tpmax;
+};
+
+})();
